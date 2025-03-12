381a5025bac61469eeba34b101bea41af2af25bc
guddnboy <hung3018@naver.com>
2025-03-12 17:29:07 +0900

    심화 답안

--- 20250312/심화/06_withoutAwait.js
+++ 20250312/심화/06_withoutAwait.js
@@ -1,10 +1,16 @@
-const getUsers = () => {
-  const request = fetch('https://jsonplaceholder.typicode.com/users')
-    .then((data) => data.json())
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => console.log(err));
-};
+/* fetch()
+- 비동기로 응답 가져오기
+- 결과로 Promise 반환
+- 무조건 성공(resolve() 실행 -> then()으로만 처리 가능) [reject 없음!!]
+-> response.ok 등을 이용해서 응답 상태 코드를 확인
+*/
 
-getUsers();
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => {
+    if (response.ok) {
+      return response.json(); // Promise 반환
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  });
