23f0eb5483a3b20db9b26b48be6f48c67bcd9fe7
guddnboy <hung3018@naver.com>
2025-03-25 17:17:18 +0900

    0325 기본 [프로젝트 세팅]

---
+++ 02_vue/20250325/기본/test-vue-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CheckboxItem />
+    <CheckboxItem />
+    <CheckboxItem />
+    <CheckboxItem />
+  </div>
+</template>
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+
+export default {
+  name: 'App',
+};
+</script>
---
+++ 02_vue/20250325/기본/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,7 @@
+<template>
+  <li><input type="checkbox" />옵션 1</li>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
