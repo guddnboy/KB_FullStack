e51bcbca340b395ee0707d91ef58defc17d7993b
guddnboy <hung3018@naver.com>
2025-03-26 14:44:01 +0900

    0326 오후 실습 [조퇴전까지]

--- 02_vue/20250326/v-model-test/src/App.vue
+++ 02_vue/20250326/v-model-test/src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <h1>Teleport Text</h1>
+  <button @click="changeModal">isModal 상태변경</button>
+
+  <teleport to="#modal">
+    <Modal v-if="isModal" />
+  </teleport>
+
   <div>
     <InputName v-model:name="searchName" />
     <h3>검색어 : {{ searchName }}</h3>
@@ -7,19 +14,21 @@
   <hr />
   <h1>부모 Click Count : {{ count }}</h1>
 
-  <ClickCount v-model:cnt="count" />
+  <ClickCount />
 </template>
 
 <script>
 import { computed } from 'vue';
 import ClickCount from './components/ClickCount.vue';
 import InputName from './components/InputName.vue';
+import ChildClickCount from './components/ChildClickCount.vue';
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
-  components: { InputName, ClickCount },
+  components: { InputName, ClickCount, ChildClickCount, Modal },
   data() {
-    return { searchName: 'John', count: 0 };
+    return { searchName: 'John', count: 0, isModal: false };
   },
 
   // 하위 컴포넌트에게 제공
@@ -31,6 +40,14 @@ export default {
       // increment() 호출 시 App.vue의 count 1 증가
       increment: () => this.count++,
     };
+  },
+  methods: {
+    changeModal() {
+      this.isModal = true;
+      setTimeout(() => {
+        this.isModal = false;
+      }, 2000);
+    },
   },
 };
 </script>
--- 02_vue/20250326/v-model-test/src/components/ClickCount.vue
+++ 02_vue/20250326/v-model-test/src/components/ClickCount.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <h1>자식 Click Count : {{ cnt }}</h1>
-    <button @click="increment">클릭 시 1 증가</button>
+    <!-- <button @click="increment">클릭 시 1 증가</button> -->
   </div>
+  <ChildClickCount />
 </template>
 
 <script>
+import ChildClickCount from './ChildClickCount.vue';
+
 export default {
   name: 'ClickCount',
-
+  components: { ChildClickCount },
   // provide로 제공된 정보 삽입하기
-  inject: ['cnt', 'increment'],
+  inject: ['cnt'],
 };
 </script>
