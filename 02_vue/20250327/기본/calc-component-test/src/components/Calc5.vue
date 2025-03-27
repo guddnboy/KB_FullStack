76c8bfc2e1c43fcb9f8433250981764f3dcd4d5f
guddnboy <hung3018@naver.com>
2025-03-27 15:13:59 +0900

    0327 기본

---
+++ 02_vue/20250327/기본/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /><br />
+    결과 : {{ result }}
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  name: 'Calc4',
+
+  setup() {
+    const x = ref(0);
+
+    const result = ref(0);
+
+    watch(x, (current, old) => {
+      console.log(`현재 값 : ${current} 이전 값 : ${old}`);
+      result.value = current;
+    });
+
+    return { x, result };
+  },
+};
+</script>
