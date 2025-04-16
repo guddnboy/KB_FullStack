3bbe18dca3eadb13b7adc6bb65e02bb801153ec1
guddnboy <hung3018@naver.com>
2025-04-16 11:49:01 +0900

    0416 오전 수업

---
+++ 03_JAVA/src/0416/ch06/sec14/Car.java
@@ -0,0 +1,32 @@
+package ch06.sec14;
+
+public class Car {
+    private int speed;
+    private boolean stop;
+
+
+    public Car() {
+
+    }
+
+    public Car(int speed, boolean stop) {
+        this.speed = speed;
+        this.stop = stop;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+    }
+}
